748b3bbaf42b599d7dffa88b1c4d52083ce624df
Mitzi <46392144+embandera@users.noreply.github.com>
2021-03-13 18:05:06 -0800

    resources

--- app.py
+++ app.py
@@ -4,25 +4,33 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
-     
+
+
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
-   
+
+
 @app.route("/hello/")
 @app.route("/hello/<name>")
-def hello_there(name = None):
+def hello_there(name=None):
     return render_template(
         "hello_there.html",
         name=name)
-   
+
 # @app.route("/api/data")
 # def get_data():
 #     return app.send_static_file("data.json")
+
+# https://macfire.github.io/devops16/html/devops16_6_deploy_flask_app_w_git.html
+# https://dev.to/lordofdexterity/deploying-flask-app-on-heroku-using-github-50nh
+# https://www.freecodecamp.org/news/how-to-build-a-web-app-using-pythons-flask-and-google-app-engine-52b1bb82b221/
